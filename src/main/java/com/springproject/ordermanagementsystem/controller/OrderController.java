ca630dbcdd300b67087dd11f00876270191f5823
Liza <lixia101111@gmail.com>
2023-02-18 00:28:32 +0200

    Add list and create order in controller and setup order page

---
+++ src/main/java/com/springproject/ordermanagementsystem/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.springproject.ordermanagementsystem.controller;
+
+import com.springproject.ordermanagementsystem.entity.Order;
+import com.springproject.ordermanagementsystem.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+@Controller
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/orders")
+    public String showOrderList(Model model) {
+        List<Order> orderList = orderService.listAlLOrder();
+        model.addAttribute("listOrders", orderList);
+        return "order";
+    }
+
+    @GetMapping("/orders/new")
+    public String addNewOrder(Model model) {
+        model.addAttribute("order", new Order());
+        model.addAttribute("pageTitle", "Add New Order");
+        return "add_order";
+    }
+
+    @PostMapping("/orders/save")
+    public String saveOrder(Order order, RedirectAttributes attributes) {
+        orderService.createOrder(order);
+        attributes.addFlashAttribute("message", "The new order has been saved!");
+        return "redirect:/orders";
+    }
+
+}
