b631e4317bc3a7b293d02ef0eba22e57f3384354
Liza <lixia101111@gmail.com>
2023-02-18 02:08:19 +0200

    Add save customer test

--- src/test/java/com/springproject/ordermanagementsystem/OrderManagementSystemApplicationTests.java
+++ src/test/java/com/springproject/ordermanagementsystem/OrderManagementSystemApplicationTests.java
@@ -1,13 +1,29 @@
 package com.springproject.ordermanagementsystem;
 
+import com.springproject.ordermanagementsystem.entity.Customer;
+import com.springproject.ordermanagementsystem.repository.CustomerRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderManagementSystemApplicationTests {
+	@Autowired
+	private CustomerRepository customerRepository;
 
 	@Test
-	void contextLoads() {
+	public void saveCustomerTest() {
+		Customer customer = new Customer();
+		customer.setId(1L);
+		customer.setFullName("Xia Li");
+		customer.setRegistrationCode("4505370");
+		customer.setEmail("liza@gmail.com");
+		customer.setTelephone("2387057097");
+
+		Customer savedCustomer = customerRepository.save(customer);
+		Assertions.assertThat(savedCustomer).isNotNull();
+		Assertions.assertThat(savedCustomer.getId()).isGreaterThan(0);
 	}
 
 }
