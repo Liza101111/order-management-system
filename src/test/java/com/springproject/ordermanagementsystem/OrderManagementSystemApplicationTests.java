c847f28cbeb2d2d9c9b15ee40718e8999f1468ce
Liza <lixia101111@gmail.com>
2023-02-18 03:29:15 +0200

    Add delete customer test

--- src/test/java/com/springproject/ordermanagementsystem/OrderManagementSystemApplicationTests.java
+++ src/test/java/com/springproject/ordermanagementsystem/OrderManagementSystemApplicationTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 @SpringBootTest
 class OrderManagementSystemApplicationTests {
 	@Autowired
@@ -24,6 +26,27 @@ class OrderManagementSystemApplicationTests {
 		Customer savedCustomer = customerRepository.save(customer);
 		Assertions.assertThat(savedCustomer).isNotNull();
 		Assertions.assertThat(savedCustomer.getId()).isGreaterThan(0);
+	}
+
+/*
+	@Test
+	public void listAllCustomerTest() {
+		Iterable<Customer> customers = customerRepository.findAll();
+
+		Assertions.assertThat(customers).hasSizeGreaterThan(0);
+		for(Customer customer: customers){
+			System.out.println(customer);
+		}
+
+	}
+*/
+	@Test
+	public void deleteCustomerTest(){
+		Long customerId = Long.valueOf(4);
+		customerRepository.deleteById(customerId);
+
+		Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+		Assertions.assertThat(optionalCustomer).isNotPresent();
 	}
 
 }
